27a5e2d8d512d2ebdc348306c7fb6637de27b40e
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2024-10-19 22:10:27 +0600

    WIP Dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', [Auth\LoginController::class, 'index']);
+Route::get('dashboard', [DashboardController::class, 'index']);
